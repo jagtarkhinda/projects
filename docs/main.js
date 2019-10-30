809b5f05f3e5dd517fc5128cdd61a21c9e1241eb
jagtarkhinda <jagtar.ca@gmail.com>
2019-10-30 14:21:30 -0400

    changes

--- docs/main.js
+++ docs/main.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
 /*******SCROLLING PAGE UP*******/
 $(document).ready(function () {
     $("#uparrow").on('click', function (event) {
-        if ($('body').height() <= ($(window).scrollTop() -20)) {
+        if ($('body').height() <= ($(window).scrollTop() + 20)) {
             $('html, body').animate({
                 scrollTop: $('#top').offset().top
             }, 800, function () {
@@ -85,25 +85,60 @@ $(document).ready(function () {
 
 /* MODAL JS */
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+var modal5 = document.getElementById("myModal5");
+var modal6 = document.getElementById("myModal6");
 
 // Get the button that opens the modal
 var btn = document.getElementsByClassName("detail");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
 // When the user clicks on the button, open the modal
-$(".detail").on('click', function (event) {
-  modal.style.display = "block";
+$(".dt1").on('click', function (event) {
+  modal1.style.display = "block";
 });
+$(".dt2").on('click', function (event) {
+    modal2.style.display = "block";
+  });
+  $(".dt3").on('click', function (event) {
+    modal3.style.display = "block";
+  });
+  $(".dt4").on('click', function (event) {
+    modal4.style.display = "block";
+  });
+  $(".dt5").on('click', function (event) {
+    modal5.style.display = "block";
+  });
+  $(".dt6").on('click', function (event) {
+    modal6.style.display = "block";
+  });
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+$(".close").on('click', function (event) {
+  modal1.style.display = "none";
+  modal2.style.display = "none";
+  modal3.style.display = "none";
+  modal4.style.display = "none";
+  modal5.style.display = "none";
+  modal6.style.display = "none";
+});
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modal1 || event.target == modal2 ||
+    event.target == modal3 ||
+    event.target == modal4 ||
+    event.target == modal5 ||
+    event.target == modal6) {
+    modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+    modal5.style.display = "none";
+    modal6.style.display = "none";
   }
 }
